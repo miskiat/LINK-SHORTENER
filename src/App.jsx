a504b484104292df4cc4a2e14d4976aa82f47b2d
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-04-03 23:40:21 +0100

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>Simplify your links</div>
+      <input placeholder="" type="link" />
+    </div>
+  );
 }
 
 export default App;
