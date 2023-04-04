6beb1b7d6cac1f0ab84bfb4abf05aed676b96d95
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-04-04 23:19:17 +0100

    link

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <div>Simplify your links</div>
-      <input placeholder="" type="link" />
+      <input className="" placeholder="Your Original URL here " type="link" />
     </div>
   );
 }
