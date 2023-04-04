24cbe15c702f3dc9842a7e5e5f47cf8dc0f126fb
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-04-04 23:35:30 +0100

    header

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <h1>Misky's URL Shortener</h1>
       <div>Simplify your links</div>
       <div className="linkk">
         <input className="" placeholder="Your Original URL here " type="link" />
         <button>SHORTEN URL</button>
       </div>
+      <p>
+        All Misky's URLs and click analytics are public and can be accessed by
+        anyone
+      </p>
     </div>
   );
 }
